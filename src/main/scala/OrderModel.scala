1fd4e1540b293645b10108717d68d11d595af551
Debasish Ghosh <dghosh@acm.org>
2012-02-12 19:46:32 +0530

    upgraded to sbt 0.11.2 and added some semigroup instances

--- src/main/scala/OrderModel.scala
+++ src/main/scala/OrderModel.scala
@@ -1,13 +1,5 @@
 package net.debasishg.domain.trade.dsl
 
-/**
- * Created by IntelliJ IDEA.
- * User: debasish
- * Date: 24/12/10
- * Time: 10:36 PM
- * To change this template use File | Settings | File Templates.
- */
-
 import java.util.{Date, Calendar}
 import scalaz._
 import Scalaz._
@@ -15,6 +7,30 @@ import Scalaz._
 trait OrderModel {this: RefModel =>
   case class LineItem(ins: Instrument, qty: BigDecimal, price: BigDecimal)
   case class Order(no: String, date: Date, customer: Customer, items: List[LineItem])
+
+  implicit def BigDecimalSemigroup: Semigroup[BigDecimal] = semigroup(_ + _)
+  type QtyPrice = (BigDecimal, BigDecimal)
+
+  implicit def QtyPriceSemigroup: Semigroup[QtyPrice] = semigroup {
+    (qp1, qp2) => {
+      val (qty1, price1) = qp1
+      val (qty2, price2) = qp2
+      (qty1 + qty2, (qty1 * price1 + qty2 * price2) / (qty1 + qty2))
+    }
+  }
+
+  implicit def OrderSemigroup =
+    new Semigroup[Order] {
+      def append(o1: Order, o2: => Order) = {
+        val Order(no1, dt1, c1, is1) = o1
+        val Order(no2, dt2, c2, is2) = o2
+        val items =
+          (is1 ++ is2).foldLeft(Map.empty[Instrument, QtyPrice])((a, i) => 
+            a + (i.ins -> a.get(i.ins).map(_ |+| (i.qty, i.price)).getOrElse((i.qty, i.price))))
+        val lineItems = items.map{ case(k, v) => LineItem(k, v._1, v._2) }
+        Order(no1 |+| no2, dt1, c1, lineItems.toList)
+      }
+    }
 
   def fromClientOrders(cos: List[Map[String, String]]) = {
     cos map {co =>
